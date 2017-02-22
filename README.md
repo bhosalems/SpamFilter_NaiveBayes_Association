40088f4a27488adcc4d08397654587438768bc3b
MaheshBhosale <maheshbhosale24051996@gmail.com>
2017-02-22 08:20:30 +0530

    Added basic spam filtering

---
+++ README.md
@@ -0,0 +1,3 @@
+# SpamFilter_NaiveBayes_Associations
+
+This project is implementation of Naive Bayes in accordance with Association Rules Mining for spam filtering.
--- spamfilter.py
+++ spamfilter.py
@@ -10,3 +10,17 @@ def run_online(classifier, setting):
     if (len(features) == 0):
         break
     print (classifier.classify(features))
+
+if __name__ == "__main__":
+    soam =sf.init_lists('enrn1/sapam/')
+    ham = sf.init_lists('enron1/ham/')
+    all_emails = [(email, 'spam') for email in spam]
+    all_emails += [(email, 'ham') for email in ham]
+    random.shuffle(all_emails)
+    print ('Corpus size = ' + str(len(all_emails)) + ' emails')
+
+    all_features = [(sf.get_features(email, ''), label) for (email, label) in all_emails]
+    train_set, test_set, classifier = sf.train(all_features, 1.0)
+
+    #classify your new email
+    run_online(classifier, "")
