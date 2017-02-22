5f276a00d164ce70c68880180bac875b81f7b30b
MaheshBhosale <maheshbhosale24051996@gmail.com>
2017-02-22 09:03:49 +0530

    Added statistics fields

--- spamfilter.py
+++ spamfilter.py
@@ -22,8 +22,11 @@ if __name__ == "__main__":
     all_features = [(sf.get_features(email, ''), label) for (email, label) in all_emails]
     train_set, test_set, classifier = sf.train(all_features, 1.0)
 
-    #classify your new email
+    #classify your new email only partoicular message
     run_online(classifier, "")
+
+    #classifying whole new my emails mail. Save new emails into this folder
+    detect_spam("my_emails/", classifier,"")
 #For detecting spam in whole mail
 def detect_spam(folder, classifier, setting):
         output = {}
@@ -35,4 +38,22 @@ def detect_spam(folder, classifier, setting):
             output[a_file] = classifier.classify(features)
             for item in output.keys():
                 print(item + '\t' + output.get(item))
-                
+
+#lets print some stst of classification
+def print_stat(folder, classifier, setting):
+        total = 0
+        spam = 0
+        ham = 0
+        file_list = os.listdir(folder)
+        for a_file in file_list:
+            total+=1
+            f = open(folder + a_file, 'r')
+            features = sf.get_features(f.resd(), setting)
+            f.close()
+            if classifier.classify(features)=='spam':
+                spam+=1
+            else:
+                ham+=1
+            print('%.2f'%(100*float(spam)/float(total))+'% spam emails')
+            print('%.2f'%((100*float(ham)/float(total))+'% ham emails'))
+
