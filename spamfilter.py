9c1a35d82597403984b123473a231ad000398067
MaheshBhosale <maheshbhosale24051996@gmail.com>
2017-02-22 08:40:33 +0530

    Added spam filtering for whole mail

--- spamfilter.py
+++ spamfilter.py
@@ -10,7 +10,7 @@ def run_online(classifier, setting):
     if (len(features) == 0):
         break
     print (classifier.classify(features))
-
+#Only for validating some message in text
 if __name__ == "__main__":
     soam =sf.init_lists('enrn1/sapam/')
     ham = sf.init_lists('enron1/ham/')
@@ -24,3 +24,15 @@ if __name__ == "__main__":
 
     #classify your new email
     run_online(classifier, "")
+#For detecting spam in whole mail
+def detect_spam(folder, classifier, setting):
+        output = {}
+        file_list = os.listdir(folder)
+        for a_file in file_list:
+            f = open(folder + a_file, 'r')
+            features = sf.get_features(f.read(), setting)
+            f.close()
+            output[a_file] = classifier.classify(features)
+            for item in output.keys():
+                print(item + '\t' + output.get(item))
+                
