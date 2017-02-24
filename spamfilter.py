6838fea669c0971071a629827c5c0236df6d9039
MaheshBhosale <maheshbhosale24051996@gmail.com>
2017-02-24 08:55:51 +0530

    Done with naive bayes

--- spamfilter.py
+++ spamfilter.py
@@ -11,6 +11,45 @@ def run_online(classifier, setting):
         break
     print (classifier.classify(features))
 #Only for validating some message in text
+#For detecting spam in whole mail
+def detect_spam(folder, classifier, setting):
+        output = {}
+        file_list = os.listdir(folder)
+        for a_file in file_list:
+            f = open(folder + a_file, 'r')
+            features = sf.get_features(f.read(), setting)
+            f.close()
+            output[a_file] = classifier.classify(features)
+            for item in output.keys():
+                print(item + '\t' + output.get(item))
+
+#lets print some stst of classification
+def print_stat(folder, classifier, setting):
+        total = 0
+        spam = 0
+        ham = 0
+        file_list = os.listdir(folder)
+        for a_file in file_list:
+            total+=1
+            f = open(folder + a_file, 'r')
+            features = sf.get_features(f.resd(), setting)
+            f.close()
+            if classifier.classify(features)=='spam':
+                spam+=1
+            else:
+                ham+=1
+            print('%.2f'%(100*float(spam)/float(total))+'% spam emails')
+            print('%.2f'%((100*float(ham)/float(total))+'% ham emails'))
+
+def explore_feats(dataset):
+    stoplist = stopwords.words('english')
+    lemmatizer = WordNetLemmatizer()
+    words = []
+    for email in dataset:
+        words += [lemmatizer.lemmatize(word.lower()) for word in
+        word_tokenize(unicode(email, errors='ignore')) if not word.lower() in stoplist]
+    fdist = nltk.FreqDist(words)
+    fdist.plot(75, cumulative=True)
 if __name__ == "__main__":
     spam = sf.init_lists('enron1/spam/')
     ham = sf.init_lists('enron1/ham/')
@@ -51,42 +90,3 @@ if __name__ == "__main__":
     print_stat('enron2/ham/', classifier, "")
     print('SPAM:')
     print_stat('enron2/spam/', classifier, "")
-#For detecting spam in whole mail
-def detect_spam(folder, classifier, setting):
-        output = {}
-        file_list = os.listdir(folder)
-        for a_file in file_list:
-            f = open(folder + a_file, 'r')
-            features = sf.get_features(f.read(), setting)
-            f.close()
-            output[a_file] = classifier.classify(features)
-            for item in output.keys():
-                print(item + '\t' + output.get(item))
-
-#lets print some stst of classification
-def print_stat(folder, classifier, setting):
-        total = 0
-        spam = 0
-        ham = 0
-        file_list = os.listdir(folder)
-        for a_file in file_list:
-            total+=1
-            f = open(folder + a_file, 'r')
-            features = sf.get_features(f.resd(), setting)
-            f.close()
-            if classifier.classify(features)=='spam':
-                spam+=1
-            else:
-                ham+=1
-            print('%.2f'%(100*float(spam)/float(total))+'% spam emails')
-            print('%.2f'%((100*float(ham)/float(total))+'% ham emails'))
-
-def explore_feats(dataset):
-    stoplist = stopwords.words('english')
-    lemmatizer = WordNetLemmatizer()
-    words = []
-    for email in dataset:
-        words += [lemmatizer.lemmatize(word.lower()) for word in
-        word_tokenize(unicode(email, errors='ignore')) if not word.lower() in stoplist]
-    fdist = nltk.FreqDist(words)
-    fdist.plot(75, cumulative=True)
