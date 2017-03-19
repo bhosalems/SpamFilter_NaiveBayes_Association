5a8f48d287df3ec93e52b3571fae67f690be6e07
MaheshBhosale <maheshbhosale24051996@gmail.com>
2017-03-19 22:35:01 +0530

    Resolved value error of unpacking values

--- naive_apripori.py
+++ naive_apripori.py
@@ -100,7 +100,6 @@ if __name__ == "__main__" :
 
     # train the our own naivebayes classifier and collect dictionary of raw probabilities of words
     t = owncl.train(all_features, 0.8)
-    print(len(t))
     train_set = t[0]
     test_set = t[1] 
     raw_spam_prob = t[2]
@@ -109,18 +108,20 @@ if __name__ == "__main__" :
     ham_total = t[5]
     spam_vocab = t[6]
     ham_vocab = t[7]
+
     #Replacing raw probabilities of frequent words
     #Using following function
     #Papri (word|spam/ ham) =nf/(napr + vocabulary)
     #nf number of occurrences of a frequent word in a coming email, napr=number of occurrences of all frequent word in a coming email.
     #vocabulary = number of words in the spam/ham final frequent item set.
     
-    for (spam_frequent_word, count) in spam_frequent:
+    for spam_frequent_word, count in spam_frequent.iteritems():
         raw_spam_prob[spam_frequent_word] = count/(spam_total+spam_vocab)
         print(raw_spam_prob[spam_frequent_word])
 
-    for (ham_frequent_word, count) in ham_frequent:
+    for ham_frequent_word, count in ham_frequent.iteritems():
         raw_ham_prob[ham_frequent_word] = count/(ham_total+ham_vocab)
-
+        print(raw_ham_prob[ham_frequent_word])
+    
     # evaluate its performance
     #evaluate(train_set, test_set, classifier)
