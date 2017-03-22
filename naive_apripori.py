429c23f9c94cbe10290e13b83c242bf883142d02
MaheshBhosale <maheshbhosale24051996@gmail.com>
2017-03-22 23:41:44 +0530

    Adding log to avoid floating point issues

--- naive_apripori.py
+++ naive_apripori.py
@@ -1,5 +1,7 @@
 from __future__ import print_function, division
+import numpy
 import nltk
+import math
 import os
 import random
 from collections import Counter
@@ -73,15 +75,15 @@ def classify(data_set, raw_spam_prob, raw_ham_prob, spam_total, ham_total, spam_
             #Handling probability of non occuring words with laplaces
                 
             try:
-                spam_prob = spam_prob*raw_spam_prob[word]
+                spam_prob = numpy.log(spam_prob) + numpy.log(raw_spam_prob[word])
             except KeyError:
                 raw_spam_prob[word]= (1/(spam_total+spam_vocab+1))
-                spam_prob = spam_prob*raw_spam_prob[word]
+                spam_prob = numpy.log(spam_prob) + numpy.log(raw_spam_prob[word])
             try:
-                ham_prob = ham_prob*raw_ham_prob[word]
+                ham_prob = numpy.log(ham_prob) + numpy.log(raw_ham_prob[word])
             except KeyError:
                 raw_ham_prob[word]= (1/(ham_total+ham_vocab+1))
-                ham_prob = ham_prob*raw_ham_prob[word]
+                ham_prob = numpy.log(ham_prob) + numpy.log(raw_ham_prob[word])
         if(spam_prob>ham_prob):
             is_spam = True
         else:
